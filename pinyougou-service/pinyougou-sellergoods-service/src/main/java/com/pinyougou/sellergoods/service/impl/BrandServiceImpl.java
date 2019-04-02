5c6405b6a0377cdd57e0712e1674f4045047dd15
xuchengfeifei@163.com <xiaoer>
2019-04-02 08:57:53 +0800

    day01

--- pinyougou-service/pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/BrandServiceImpl.java
+++ pinyougou-service/pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/BrandServiceImpl.java
@@ -1,6 +1,9 @@
 package com.pinyougou.sellergoods.service.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.github.pagehelper.ISelect;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.pinyougou.mapper.BrandMapper;
 import com.pinyougou.pojo.Brand;
 import com.pinyougou.service.BrandService;
@@ -25,7 +28,17 @@ public class BrandServiceImpl implements BrandService {
 
     @Override
     public List<Brand> findAll() {
-        System.out.println("brandMapper: " + brandMapper);
-        return brandMapper.findAll();
+        // 开始分页
+        PageInfo<Brand> pageInfo = PageHelper.startPage(1, 10)
+                .doSelectPageInfo(new ISelect() {
+                    @Override
+                    public void doSelect() {
+                        brandMapper.findAll();
+                    }
+                });
+        System.out.println("总记录数：" + pageInfo.getTotal());
+        System.out.println("总页数：" + pageInfo.getPages());
+        return pageInfo.getList();
+
     }
 }
